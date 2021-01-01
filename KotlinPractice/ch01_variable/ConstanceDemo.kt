92fe93347d5c384daebcb49e3079210dbaa7f6d3
yoonbr <yoon_br@naver.com>
2021-01-01 22:50:11 +0900

    20210101

---
+++ KotlinPractice/ch01_variable/ConstanceDemo.kt
@@ -0,0 +1,9 @@
+package ch01_variable
+
+// 상수(Constance) : 변하지 않는 변수, 읽기 전용 변수
+
+fun main(){
+    val MAX : Int = 100
+    // MAX = 200 // error 중간에 변할 수 없음
+    println(MAX)
+}
---
+++ KotlinPractice/ch01_variable/Literal.kt
@@ -0,0 +1,11 @@
+package ch01_variable
+
+// literal : 어떤 값을 나타냄, 값 자체
+// 값 자체를 가지지 않으면 null literal
+
+fun main() {
+    println(1234) // 정수 리터럴
+    println(3.14F) // 실수 리터럴 (실수 형식을 나타내기 위해 F를 작성해줌)
+    println('A') // 문자 리터럴 (문자 하나)
+    println("HELLO") // 문자열 리터럴
+}
