88c79b05a3f6ba58662ea79228b2463ede7940bf
yoonbr <yoon_br@naver.com>
2021-01-17 23:40:35 +0900

    20210117

---
+++ KotlinPractice/ch15_Function/Function.kt
@@ -0,0 +1,14 @@
+package ch15_Function
+
+// 함수(Fucntion) : 반복해서 사용하도록 만들어진 하나 이상의 문을 포함하는 코드블록
+
+fun main() {
+    // 함수 호출
+    show()
+    show()
+}
+
+// 함수 선언
+fun show() {
+    println("Hello World")
+}
