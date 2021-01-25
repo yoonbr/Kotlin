46edece1744543c9e1f5126b2d05d508bb409bdd
yoonbr <yoon_br@naver.com>
2021-01-25 21:41:10 +0900

    20210125

--- 0123Note/app/src/main/java/com/yoond/a0123note/MainActivity.kt
+++ 0123Note/app/src/main/java/com/yoond/a0123note/MainActivity.kt
@@ -2,6 +2,7 @@ package com.yoond.a0123note
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebViewClient
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -9,12 +10,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        webView.loadUrl("http://wwww.naver.com")
+        webView.loadUrl("http://www.naver.com")
+        webView.webViewClient = WebViewClient()
 
+        // URL로 이동
         btngo.setOnClickListener {
             val url = addr.text.toString() // text를 String으로 변환
             webView.loadUrl(url) // 위의 url을 읽어오기
         }
+        // 뒤로 가기
         btnback.setOnClickListener {
             webView.goBack()
         }
@@ -23,7 +27,7 @@ class MainActivity : AppCompatActivity() {
         }
         btnforward.setOnClickListener {
             // app 내의 assets 디렉토리의 test.html을 로드
-            webView.loadUrl("file://android_asset/test.html")
+            webView.loadUrl("file:///android_asset/test.html")
         }
 
     }
