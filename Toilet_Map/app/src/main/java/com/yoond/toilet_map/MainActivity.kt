c4f146e5c8a1e9613b00c71510a744404276baba
yoonbr <yoon_br@naver.com>
2021-02-08 20:45:37 +0900

    20210208 google location api

---
+++ Toilet_Map/app/src/main/java/com/yoond/toilet_map/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.yoond.toilet_map
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
